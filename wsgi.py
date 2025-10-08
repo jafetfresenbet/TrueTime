a31a613fdd687dfcc4f30ec12b068a63b2b180a8
jafetfresenbet <jafet.haileslassie@gmail.com>
2025-10-08 11:56:33 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,3 @@
-import os
-import app from app
+from app import app, init_db
 
-# ---------- Starta app ----------
-if __name__ == '__main__':
-  init_db()
-  port = int(os.environ.get("PORT", 5000))
-  app.run(host="0.0.0.0", port=port)
+init_db()
