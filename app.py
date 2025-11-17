49b8ce554f469ff6690987fcd5dba46d4b619397
jafetfresenbet <jafet.haileslassie@gmail.com>
2025-11-17 22:31:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -978,7 +978,7 @@ def test_mail():
 
 @app.route("/test_sms", methods=["GET", "POST"])
 def test_sms():
-    number = "0760576531"          # e.g. "0701234567"
+    number = "46760576531"          # e.g. "0701234567"
     carrier = "sms.telia.se"        # e.g. "sms.telia.se"
     message = "Hello from Flask"        # e.g. "Hello from Flask!"
 
@@ -2026,6 +2026,7 @@ PROFILE_TEMPLATE = """
 </body>
 </html>
 """
+
 
 
 
