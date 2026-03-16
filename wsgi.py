df4800e709a0306f2a8ab1dbd0869fa27bcea6f4
jafetfresenbet <jafet.haileslassie@gmail.com>
2026-03-16 23:04:42 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -12,4 +12,4 @@ if __name__ == '__main__':
     # Hämta port från miljövariabler (viktigt för servrar)
     port = int(os.environ.get("PORT", 5000))
     # host="0.0.0.0" gör att den lyssnar på externa anrop
-    app.run(host="0.0.0.0", port=port, debug=True)True)
+    app.run(host="0.0.0.0", port=port, debug=True)
